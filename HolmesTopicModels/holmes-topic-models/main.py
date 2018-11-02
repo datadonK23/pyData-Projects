f522fe76447d169ef253e4895f026cb1c2958131
datadonK23 <datadonk23@gmail.com>
2018-11-02 14:03:53 +0100

    Main logic and plot result

--- HolmesTopicModels/holmes-topic-models/main.py
+++ HolmesTopicModels/holmes-topic-models/main.py
@@ -7,53 +7,63 @@ Author: datadonk23
 Date: 23.10.18 
 """
 
+import numpy as np
 import pandas as pd
+from tabulate import tabulate
 
-from preprocessing import load_corpus, tfidf_vectorizer
+from preprocessing import Collections, load_corpus, tfidf_vectorizer
 from model import make_pipeline, make_model
 
 
-def main():
-    """
-    Main Loop
+def plot_df(corpus, topic_distribution, n_topics=5):
+    """ Output: Plot Dataframe to CLI
+
+    :param corpus: Bunch - Text corpus
+    :param topic_distribution: array - Topic distribution for each doc
+    :param n_topics: int - Number of topics in topic model
     :return: -
     """
 
+    orig_collections = Collections().original
+    novel_collections = Collections().novel
+
+    topics = ["Topic" + str(i) for i in range(n_topics)]
+    docs = [" ".join(f_name.split("/")[-1].split(".")[0].split("_"))
+            for f_name in corpus.filenames]
+
+    df = pd.DataFrame(np.round(topic_distribution, 3),
+                      columns=topics, index=docs)
+    df["assigned_topic"] = np.argmax(df.values, axis=1)
+    df["orig_collection"] = [orig_collections.get(item, item)
+                             for item in corpus.target]
+    df["novel_collection"] = [novel_collections.get(item, item)
+                              for item in df.assigned_topic.values]
+
+    df_assignment = df.sort_values("assigned_topic").loc[:,
+                    ["orig_collection", "novel_collection"]]
+    df_assignment.columns = ["Original Collection", "Novel Collection"]
+    print(tabulate(df_assignment, headers="keys", tablefmt="fancy_grid"))
+
+
+def main():
+    """ Main loop
+
+    :return: -
+    """
+    
+    n_topics = 5
     corpus = load_corpus("data/")
 
     # NLP pipeline
     tfidf = tfidf_vectorizer()
-    model = make_model(n_topics=5)
+    model = make_model(n_topics=n_topics)
 
+    # Training
     pipeline = make_pipeline(tfidf, model)
     topic_distr = pipeline.fit_transform(corpus.data)
 
-
-
-    #
-    #
-    # print("Novel Sherlock Holmes Short Stories Collections:")
-    # for _, title in novel_collections_map.items():
-    #     print("*", title)
-    #
-    # topics = ["Topic" + str(i) for i in range(n_topics)]
-    # docs = [" ".join(f_name.split("/")[-1].split(".")[0].split("_"))
-    #         for f_name in corpus.filenames]
-    #
-    # df_document_topic = pd.DataFrame(np.round(nmf_topic_distr, 3),
-    #                                  columns=topics, index=docs)
-    # df_document_topic["assigned_topic"] = np.argmax(df_document_topic.values,
-    #                                                 axis=1)
-    # df_document_topic["orig_collection"] = [collections_map.get(item, item) for
-    #                                         item in corpus.target]
-    # df_document_topic["novel_collection"] = [
-    #     novel_collections_map.get(item, item)
-    #     for item in df_document_topic.assigned_topic.values]
-    #
-    # df_novel_assignment = df_document_topic.sort_values("assigned_topic").loc[:,
-    #                       ["orig_collection",
-    #                        "novel_collection"]]
-    # df_novel_assignment
+    # Output
+    plot_df(corpus, topic_distr, n_topics)
 
 
 if __name__ == "__main__":
